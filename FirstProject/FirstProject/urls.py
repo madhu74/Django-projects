00001ed807c385870ecb1ba1cb2bdbf3accaf45a
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-13 04:03:28 -0500

    reverted back

--- FirstProject/FirstProject/urls.py
+++ FirstProject/FirstProject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     #url(r'^$',views.mindex,name='mindex'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^music',include('music.urls')),
 ]
