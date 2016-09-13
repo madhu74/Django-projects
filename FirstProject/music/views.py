90aefb29d78f2ee8e4fbefbbcdb01a826bf433f2
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-13 01:35:27 -0500

    creating a response view for the url

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(response):
+    return HttpResponse("<h1>Home Page for Music app</h1>")
