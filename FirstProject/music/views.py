f5ba45cef813d215d45660f740ca125d5719d31d
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-14 02:53:18 -0500

    second view added

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse("<h1>Home Page for Music app</h1>")
+
+def detail(response,album_id):
+    return HttpResponse("<h1>Accessing album id "+str(album_id)+"</h1>")
