ab735b7e7f7137c28a120bd5bb09f87e19c8dd14
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-14 02:00:22 -0500

    trials

--- FirstProject/FirstProject/urls.py
+++ FirstProject/FirstProject/urls.py
@@ -19,7 +19,7 @@ from . import views
 urlpatterns = [
 
     url(r'^$',views.mindex,name='mindex'),
-    url(r'^music', include('music.urls')),
+    url(r'^music+', include('music.urls')),
     url(r'^admin/', admin.site.urls),
 
 ]
