36842d6f1a740e37f311bbc62aaec1e0b2d4c51e
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-15 18:00:02 -0500

    updated detail file

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.http import Http404
 from . models import Album
+from django.shortcuts import render
 from django.template import loader
 
 # def index(response):
@@ -20,4 +22,9 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request,album_id):
-    return HttpResponse("<h1>Details of album id :"+str(album_id)+"</h1>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    return render(request,'music/detail.html',{'album' : album})
