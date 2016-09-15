43ef86c80c33947a38f85b3b0e28a86fca358092
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-14 19:34:46 -0500

    Adding the DB in the views

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -5,10 +5,10 @@ def index(response):
     html_tag=""
 
     for album in Album.objects.all():
-        url="/music"+str(album.id)+"/"
+        url="/music/"+str(album.id)+"/"
         html_tag+='<a href="'+ url +'">'+album.album_name+"</a><br>"
 
     return HttpResponse(html_tag)
 
 def detail(response,album_id):
-    return HttpResponse("<h1>Details of album id "+str(album_id)+"</h1>")
+    return HttpResponse("<h1>Details of album id :"+str(album_id)+"</h1>")
