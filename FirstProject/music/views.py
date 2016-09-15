48b02818745ce94c7982b0f818fee0cf104c039d
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-14 19:32:10 -0500

    Adding Url files

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from . models import Album
 
 def index(response):
-    return HttpResponse("<h1>Home Page for Music app</h1>")
+    html_tag=""
+
+    for album in Album.objects.all():
+        url="/music"+str(album.id)+"/"
+        html_tag+='<a href="'+ url +'">'+album.album_name+"</a><br>"
+
+    return HttpResponse(html_tag)
 
 def detail(response,album_id):
-    return HttpResponse("<h1>Accessing album id "+str(album_id)+"</h1>")
+    return HttpResponse("<h1>Details of album id "+str(album_id)+"</h1>")
