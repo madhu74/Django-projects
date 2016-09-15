85d4e51b719e6b2c56c569946170ceeeffa462d6
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-09-14 23:06:06 -0500

    Adding html template

--- FirstProject/music/views.py
+++ FirstProject/music/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 from . models import Album
+from django.template import loader
 
-def index(response):
-    html_tag=""
+# def index(response):
+#     html_tag=""
+#
+#     for album in Album.objects.all():
+#         url="/music/"+str(album.id)+"/"
+#         html_tag+='<a href="'+ url +'">'+album.album_name+"</a><br>"
+#
+#     return HttpResponse(html_tag)
 
-    for album in Album.objects.all():
-        url="/music/"+str(album.id)+"/"
-        html_tag+='<a href="'+ url +'">'+album.album_name+"</a><br>"
+def index(request):
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        "all_albums":all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
-    return HttpResponse(html_tag)
-
-def detail(response,album_id):
+def detail(request,album_id):
     return HttpResponse("<h1>Details of album id :"+str(album_id)+"</h1>")
